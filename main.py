42a1cde939c4a1dbebea983fa176b7e70a63fba0
Mariana <haroldc2005@gmail.com>
2024-08-29 01:37:03 -0500

    refactor: cambia comillas docles por simples.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from datetime import datetime
 import json
 
-
 # Own libraries
 from fastapi import FastAPI
 import uvicorn
@@ -10,7 +9,7 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/lista-ordenada")
+@app.get('/lista-ordenada')
 def lista_ordenada(lista_no_ordenada: str) -> dict:
     """
     Ordena una lista de números y devuelve la lista ordenada junto con la hora actual del sistema.
@@ -19,21 +18,21 @@ def lista_ordenada(lista_no_ordenada: str) -> dict:
         lista_no_ordenada: Lista de números enteros no ordenada en formato JSON.
 
     Returns:
-        La hora actual del sistema en formato "YYYY-MM-DD HH:MM:SS" y la
+        La hora actual del sistema en formato 'YYYY-MM-DD HH:MM:SS' y la
         lista de números ordenada de menor a mayor.
 
     """
     lista_no_ordenada = json.loads(lista_no_ordenada)
     lista_ordenada = sorted(lista_no_ordenada)
 
-    hora_sistema = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    hora_sistema = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     return {
-        "hora_sistema": hora_sistema,
-        "lista_ordenada": lista_ordenada
+        'hora_sistema': hora_sistema,
+        'lista_ordenada': lista_ordenada
     }
 
-@app.get("/healthcheck")
+@app.get('/healthcheck')
 def healthcheck() -> str:
     """
     Verifica el estado de la API.
@@ -41,7 +40,7 @@ def healthcheck() -> str:
     Returns:
         Cadena que indica que la API está funcionando correctamente.
     """
-    return "OK"
+    return 'OK'
 
 if __name__ == '__main__':
     uvicorn.run(app)
